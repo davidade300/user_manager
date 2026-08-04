9b8cc04b0307b737608201aeb8b09fe707eed663
davidade300 <iaderinho@icloud.com>
2026-08-04 17:02:08 -0300

    testes e implementação para use case de atualizacao de nome completo.

---
+++ tests/unit/test_update_full_name_use_case.py
@@ -0,0 +1,50 @@
+import pytest
+
+from user_manager.core.domain.exceptions import InsufficientPrivileges
+from user_manager.core.use_cases.update_full_name import UpdateUserFullName
+
+
+@pytest.fixture
+def update_user_full_name(user_repository, regular_user) -> UpdateUserFullName:
+    user_repository.save(regular_user)
+
+    update_user_full_name: UpdateUserFullName = UpdateUserFullName(
+        user_repository
+    )
+    return update_user_full_name
+
+
+class TestUpdateUserFullName:
+    def test_admin_can_update_regular_user_full_name(
+        self, regular_user, admin_user, user_repository, update_user_full_name
+    ) -> None:
+
+        update_user_full_name.execute(
+            actor=admin_user, user_id=regular_user.id, new_full_name='new_name'
+        )
+        assert (
+            user_repository.get_by_id(regular_user.id).full_name == 'new_name'
+        )
+
+    def test_regular_user_can_update_its_own_full_name(
+        self, regular_user, user_repository, update_user_full_name
+    ) -> None:
+
+        update_user_full_name.execute(
+            actor=regular_user,
+            user_id=regular_user.id,
+            new_full_name='new_name',
+        )
+        assert (
+            user_repository.get_by_id(regular_user.id).full_name == 'new_name'
+        )
+
+    def test_regular_user_cant_update_others_full_name(
+        self, regular_user, user_repository, admin_user, update_user_full_name
+    ) -> None:
+        with pytest.raises(InsufficientPrivileges):
+            update_user_full_name.execute(
+                actor=regular_user,
+                user_id=admin_user.id,
+                new_full_name='new_name',
+            )
